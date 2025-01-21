a6238750d4c7d7954d6ea6ccbe16fe3cf6ff2693
NakePranob <pranob.onphaeng@gmail.com>
2025-01-21 16:14:06 +0700

    add authorization and login pages with AWS WAF captcha integration

---
+++ pages/authorize.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const runtimeConfig = useRuntimeConfig();
+const route = useRoute();
+const queryParams = route.query;
+
+onMounted(() => {
+    const fullUri = `?${Object.entries(queryParams)
+        .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`)
+        .join('&')}`;
+    console.log(fullUri);
+    window.location.href = `${runtimeConfig.public.apiBase}/api/v1/auth/authorize${fullUri}`;
+})
+</script>
