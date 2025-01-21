86c57c31854d770233e11b52d59a024040265a55
NakePranob <pranob.onphaeng@gmail.com>
2025-01-21 16:59:08 +0700

    fix: update Google reCAPTCHA script type in nuxt.config for better compatibility

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default defineNuxtConfig({
       script: [
         {
           src: 'https://www.google.com/recaptcha/api.js?render=explicit',
-          async: true,
+          type: 'text/javascript',
           defer: true,
         },
       ]
